86641bfc41a3c7ed3ae6476d6ae54256b83b580c
LexxxusPtitsa <alekptitsa@yandex.ua>
2020-07-02 01:45:12 +0300

    second

---
+++ src/static/js/script.js
@@ -0,0 +1,7 @@
+document.querySelector('.header__icon--delete').addEventListener('click', function () {
+    document.querySelector('.modal').classList.add('modal--active');
+});
+document.querySelector('.modal__button--cancel').addEventListener('click', function () {
+    document.querySelector('.modal').classList.remove('modal--active');
+});
+
